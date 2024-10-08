181797f7c5879362b1fc102e38b063bdbb2b4166
Tioyos <147788269+Tioyos@users.noreply.github.com>
2024-10-08 11:35:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/*exarcicio 1
+function saudacao(nome) {
+console.log(`ola ${nome}`);
+}
+
+let nome = ["ola claudio"]
+console.log(nome[0]);
+*/
+
+/*exercicio 2
+
+function soma (a, b ){
+    return a + b
+}    
+    console.log(soma(2, 4));
+*/
